96db4382a84f0032b843bacb7fb00372fe8fecbc
machinejun <akswns03@naver.com>
2022-06-14 11:20:34 +0900

    eventActivityRepack

--- project/app/src/main/java/com/example/project/EventActivity.java
+++ project/app/src/main/java/com/example/project/EventActivity.java
@@ -19,12 +19,11 @@ public class EventActivity extends AppCompatActivity{
     private ImageView randomMuchineImg;
     private ImageView radomballImg;
     private Handler handler;
-
+    private Message message;
     private EventActivity context;
 
-    public EventActivity() {
+    private EventActivity() {
     }
-
     public static EventActivity newInstance() {
         if(instance == null){
             instance = new EventActivity();
@@ -48,6 +47,7 @@ public class EventActivity extends AppCompatActivity{
 
 
         /**
+         * 메세지를 수신하여 동작을 처리
          * 여기다가 MainActivity.class 에다가 바꿔 넣을꺼 끼면 됨
          */
         handler = new Handler(new Handler.Callback() {
@@ -72,7 +72,9 @@ public class EventActivity extends AppCompatActivity{
             }
         });
 
-
+        /**
+         * 이벤트 처리
+         */
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -89,44 +91,72 @@ public class EventActivity extends AppCompatActivity{
                             randomMuchineImg.setImageResource(R.drawable.machine4);
                             break;
                     }
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+                    sleepThread(0);
+                }
+
+                for(int i = 1; i < 5; i++){
+                    switch (i){
+                        case 1:
+                            sentMessage(i);
+                            Animation slideAnimation = AnimationUtils
+                                    .loadAnimation(context, R.anim.translate_anim);
+                            radomballImg.startAnimation(slideAnimation);
+                            sleepThread(i);
+                            break;
+                        case 2:
+                            sentMessage(i);
+                            sleepThread(i);
+                            break;
+                        case 3:
+                            sentMessage(i);
+                            sleepThread(i);
+                            break;
+                        case 4:
+                            sentMessage(i);
                     }
                 }
-                Message message = handler.obtainMessage();
-                message.what = 1;
-                handler.sendMessage(message);
-                Animation slideAnimation = AnimationUtils
-                        .loadAnimation(context, R.anim.translate_anim);
-                radomballImg.startAnimation(slideAnimation);
-                try {
-                    Thread.sleep(1200);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                message = handler.obtainMessage();
-                message.what = 2;
-                handler.sendMessage(message);
-                try {
-                    Thread.sleep(250);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                message = handler.obtainMessage();
-                message.what = 3;
-                handler.sendMessage(message);
-                try {
-                    Thread.sleep(50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                message = handler.obtainMessage();
-                message.what = 4;
-                handler.sendMessage(message);
             }
         });
         thread.start();
+    }
+
+    /**
+     * 메세지를 보내는 기능
+     * @param msgWhat (msg 내용)
+     */
+    private void sentMessage(int msgWhat){
+        message = handler.obtainMessage();
+        if(msgWhat == 1){
+            message.what = msgWhat;
+        }else if (msgWhat == 2){
+            message.what = msgWhat;
+        }else if(msgWhat == 3){
+            message.what = msgWhat;
+        }else if(msgWhat == 4){
+            message.what = msgWhat;
+        }
+        handler.sendMessage(message);
+    }
+
+    /**
+     * 쓰레드 슬립
+     * @param type
+     */
+    private void sleepThread(int type){
+        if(type == 1){
+            type = 1200;
+        }else if (type == 2){
+            type = 250;
+        }else if(type == 3){
+            type = 50;
+        }else{
+            type = 100;
+        }
+        try {
+            Thread.sleep(type);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 }
